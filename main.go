55d1a83da178cee718c1b8a53dd092bfc222b39d
haruna1256 <haru_kawagishi@icloud.com>
2025-11-03 09:21:40 +0900

    [add] 写真の情報の表示、追加

--- main.go
+++ main.go
@@ -1,17 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 	"github.com/haruna1256/Momento-backend/database"
 	"github.com/haruna1256/Momento-backend/handlers"
 )
 
 func main() {
-     // データベース接続
+    // データベース接続
     database.ConnectDB()
 
     // ルーター作成
@@ -20,6 +16,7 @@ func main() {
     // エンドポイント設定
     router.GET("/photos", handlers.GetPhotos)
     router.POST("/photos", handlers.CreatePhoto)
-    fmt.Println("Server running on http://localhost:8080")
-    log.Fatal(http.ListenAndServe(":8080", nil))
+
+    // サーバー起動
+    router.Run(":8080") // ここでサーバーを起動
 }
